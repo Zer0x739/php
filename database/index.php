45473534b266237a865892ca27816d0768477a31
Zer0x739 <76940003+Zer0x739@users.noreply.github.com>
2023-05-23 13:24:27 +0200

    Add files via upload

--- database/index.php
+++ database/index.php
@@ -12,13 +12,44 @@ if ($conn->connect_error) {
 
 $sql = "SELECT id, Jmeno, Prijimeni FROM osoby";
 $result = $conn->query($sql);
+?>
 
-if ($result->num_rows > 0) {
-  while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Jmeno: " . $row["Jmeno"]. " " . $row["Prijimeni"]. "<br>";
-  }
-} else {
-  echo "0 results";
-}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        table {
+            border-collapse: collapse;
+        }
+        table, th, td {
+            border: 1px solid black;
+            padding: 5px;
+        }
+    </style>
+</head>
+<body>
+    <?php if ($result->num_rows > 0); ?>
+        <table>
+            <tr>
+                <th>Id</th>
+                <th>Jmeno</th>
+                <th>Prijimeni</th>
+            </tr>
+            <?php while ($row = $result->fetch_assoc()): ?>
+                <tr>
+                    <td><?php echo $row["id"]; ?></td>
+                    <td><?php echo $row["Jmeno"]; ?></td>
+                    <td><?php echo $row["Prijimeni"]; ?></td>
+                </tr>
+            <?php endwhile; ?>
+        </table>   
+</body>
+</html>
+
+<?php
 $conn->close();
-?> 
+?>
